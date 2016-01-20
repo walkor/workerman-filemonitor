64412cfcceea1279415cc21971c3d9d81624ea68
walkor <walkor@users.noreply.github.com>
2016-01-20 22:13:00 +0800

    Update start.php

--- Applications/FileMonitor/start.php
+++ Applications/FileMonitor/start.php
@@ -2,49 +2,45 @@
 use Workerman\Worker;
 use Workerman\Lib\Timer;
 
-// 默认监控Workerman的Applications目录
+// watch Applications catalogue
 $monitor_dir = realpath(__DIR__.'/..');
 
 // worker
 $worker = new Worker();
-// 名称，方便status时候辨别
 $worker->name = 'FileMonitor';
-// 该进程收到reload信号不执行reload
 $worker->reloadable = false;
-// 所有文件中最新的更改时间
 $last_mtime = time();
 
-// 进程启动后安装定时器
 $worker->onWorkerStart = function()
 {
     global $monitor_dir;
-    // 只在debug模式下监控文件，守护进程模式不监控
+    // watch files only in daemon mode
     if(!Worker::$daemonize)
     {
-        // 定时检查被监控目录文件是否有修改
+        // chek mtime of files per second 
         Timer::add(1, 'check_files_change', array($monitor_dir));
     }
 };
 
-// 检查文件是否有修改
+// check files func
 function check_files_change($monitor_dir)
 {
     global $last_mtime;
-    // 递归遍历目录
+    // recursive traversal directory
     $dir_iterator = new RecursiveDirectoryIterator($monitor_dir);
     $iterator = new RecursiveIteratorIterator($dir_iterator);
     foreach ($iterator as $file)
     {
-        // 只监控php文件
+        // only check php files
         if(pathinfo($file, PATHINFO_EXTENSION) != 'php')
         {
             continue;
         }
-        // 在最近有修改
+        // check mtime
         if($last_mtime < $file->getMTime())
         {
             echo $file." update and reload\n";
-            // 给父进程发送reload信号
+            // send SIGUSR1 signal to master process for reload
             posix_kill(posix_getppid(), SIGUSR1);
             $last_mtime = $file->getMTime();
             break;
